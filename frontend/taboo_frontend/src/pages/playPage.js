9ed572cc5319a48f983f8ff551bd9675c7139b30
Finn <jcsayalak7@gmail.com>
2020-07-18 18:01:07 +1200

    Basic template for each page

--- frontend/taboo_frontend/src/pages/playPage.js
+++ frontend/taboo_frontend/src/pages/playPage.js
@@ -4,9 +4,9 @@ import { useRouteMatch } from 'react-router-dom';
 
 export const PlayPage = props => {
     const name = props.name;
-
     const route = useRouteMatch("/:id")
-    console.log(name)
+
+    
     return (
         <div>
             Hey
