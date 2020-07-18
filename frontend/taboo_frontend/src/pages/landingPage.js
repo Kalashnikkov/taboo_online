43b38e8c5427a2e4db4cd25bb4bd3913f531a6e1
Finn <jcsayalak7@gmail.com>
2020-07-19 10:09:58 +1200

    Finishing landing page

--- frontend/taboo_frontend/src/pages/landingPage.js
+++ frontend/taboo_frontend/src/pages/landingPage.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { makeStyles, Container, Paper, Grid, Button } from "@material-ui/core";
+import {
+  makeStyles,
+  Container,
+  Paper,
+  Grid,
+  Button,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+} from "@material-ui/core";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles(() => ({
   root: {
-    background: "#F5F5F5",
+    background: "#f9ca24",
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
@@ -14,9 +24,24 @@ const useStyles = makeStyles(() => ({
     width: "20vh",
     height: "10vh",
     textAlign: "center",
+    backgroundColor: "#6ab04c",
+    color: "#ffffff",
   },
   paperLayout: {
     margin: "5%",
+  },
+  barTitle: {
+    flexGrow: 1,
+  },
+  barCSS: {
+    backgroundColor: "#f0932b",
+  },
+  topBar: {
+    backgroundColor: "green",
+  },
+  containerItems: {
+    marginTop: "10%",
+    flexGrow: 1,
   },
 }));
 
@@ -24,24 +49,67 @@ export const LandingPage = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
+      <AppBar position="static" className={classes.barCSS}>
+        <Toolbar>
+          <Typography variant="h4" className={classes.barTitle}>
+            Mist Games
+          </Typography>
+          <Button color="inherit" className={classes.barTitle}>
+            All Games
+          </Button>
+          <Button color="inherit" className={classes.barTitle}>
+            Contact
+          </Button>
+          <Button color="inherit" className={classes.barTitle}>
+            About
+          </Button>
+          <Button color="inherit" className={classes.barTitle}>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
       <Container className={classes.containerItems}>
         <Grid container spacing={1}>
-          <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-          >
+          <Grid container direction="row" justify="center" alignItems="center">
             <Paper className={classes.paperLayout}>
               <Button className={classes.title} component={Link} to={"/taboo/"}>
                 Taboo!
               </Button>
             </Paper>
             <Paper className={classes.paperLayout}>
-              <Button className={classes.title}>Monopoly</Button>
+              <Button className={classes.title} disabled="true">
+                Monopoly
+              </Button>
             </Paper>
             <Paper className={classes.paperLayout}>
-              <Button className={classes.title}>Connect 4</Button>
+              <Button className={classes.title} disabled="true">
+                Connect 4
+              </Button>
+            </Paper>
+            <Paper className={classes.paperLayout}>
+              <Button className={classes.title} disabled="true">
+                Mahjong
+              </Button>
+            </Paper>
+          </Grid>
+          <Grid container direction="row" justify="center" alignItems="center">
+            <Paper className={classes.paperLayout}>
+              <Button className={classes.title} disabled="true">
+                Articulate!
+              </Button>
+            </Paper>
+            <Paper className={classes.paperLayout}>
+              <Button className={classes.title} disabled="true">
+                Catan
+              </Button>
+            </Paper>
+            <Paper className={classes.paperLayout}>
+              <Button className={classes.title} disabled="true">
+                Uno!
+              </Button>
+            </Paper>
+            <Paper className={classes.paperLayout}>
+              <Button className={classes.title}>Create your own!</Button>
             </Paper>
           </Grid>
         </Grid>
