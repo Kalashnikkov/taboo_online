391f5d9edf98edabafbc069112d8ede0ec54ba70
Lance Delos Reyes <lan_drey19@hotmail.com>
2020-07-19 00:37:42 +1200

    changed from GamePage to HomePage

--- frontend/taboo_frontend/src/App.js
+++ frontend/taboo_frontend/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { GamePage } from './pages/gamePage';
 import { LobbyPage } from './pages/lobbyPage';
 import { WinPage } from './pages/winPage';
+import { HomePage } from './pages/homePage';
 
 function App() {
   return (
     <div>
-      <GamePage/>
+      <HomePage/>
     </div>
   );
 }
