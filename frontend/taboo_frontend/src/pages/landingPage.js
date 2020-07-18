76ed5eb9601ab8072e2c965959a22e30876375a6
Finn <jcsayalak7@gmail.com>
2020-07-18 22:14:51 +1200

    Greatest changem, going home

--- frontend/taboo_frontend/src/pages/landingPage.js
+++ frontend/taboo_frontend/src/pages/landingPage.js
@@ -1,19 +1,52 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core";
-import BackgroundImage from "../assets/bg.jpg";
+import { makeStyles, Container, Paper, Grid, Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
-    backgroundImage: `url(${BackgroundImage})`,
     background: "#F5F5F5",
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
     height: "100vh",
   },
+  title: {
+    width: "20vh",
+    height: "10vh",
+    textAlign: "center",
+  },
+  paperLayout: {
+    margin: "5%",
+  },
 }));
 
 export const LandingPage = () => {
   const classes = useStyles();
-  return <div className={classes.root}></div>;
+  return (
+    <div className={classes.root}>
+      <Container className={classes.containerItems}>
+        <Grid container spacing={1}>
+          <Grid
+            container
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            <Paper className={classes.paperLayout}>
+              <Button className={classes.title} component={Link} to={"/taboo/"}>
+                Taboo!
+              </Button>
+            </Paper>
+            <Paper className={classes.paperLayout}>
+              <Button className={classes.title}>Monopoly</Button>
+            </Paper>
+            <Paper className={classes.paperLayout}>
+              <Button className={classes.title}>Connect 4</Button>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 };
